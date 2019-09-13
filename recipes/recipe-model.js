8f86f2b5659afae807755722b170f08f89d04d9b
Pat Palmerston <patpalmerston@gmail.com>
2019-09-12 21:24:31 -0700

    getInstructions up

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -1,9 +1,25 @@
 const db = require('../data/dbConfig')
 
 module.exports = {
-  getRecipes
+  getRecipes,
+  getShoppingList,
+  getInstructions,
 }
 
 function getRecipes() {
   return db('recipes')
+}
+
+//should return a list of all ingredients and quantities for a given recipe
+function getShoppingList(recipe_id) {
+  console.log('getShoppingList')
+}
+
+//should return a list of step by step instructions for preparing a recipe
+function getInstructions(id) {
+ 
+  return db('steps as s')
+    .join('recipes as r', 'r.id', 's.recipe_id')
+    .select('s.id', 'r.recipe_name', 's.step_number', 's.instructions')
+    .where('s.recipe_id', id)
 }
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,18 +1,36 @@
-const express = require('express')
+const express = require('express');
 
-const Recipes = require('./recipe-model')
+const Recipes = require('./recipe-model');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  Recipes.getRecipes()
-    .then(recipes => {
-      res.json(recipes)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({message: 'Failed to get recipes'})
-    })
-})
+	Recipes.getRecipes()
+		.then(recipes => {
+			res.json(recipes);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ message: 'Failed to get recipes' });
+		});
+});
 
-module.exports = router
+router.get('/:id/steps', (req, res) => {
+	const { id } = req.params;
+
+	Recipes.getInstructions(id)
+		.then(steps => {
+			if (steps) {
+				res.json(steps);
+			} else {
+				res
+					.status(404)
+					.json({ message: 'Could not find Instructions for Recipe' });
+			}
+		})
+		.catch(err => {
+			res.status().json({ message: 'Failed to get step by step instructions' });
+		});
+});
+
+module.exports = router;
